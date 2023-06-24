adcb7f531fce240d0c04c09b61081fbb2560c7db
Aiman <aiman.bscsf19@iba-suk.edu.pk>
2023-06-24 16:05:53 +0500

    Added cors to handle errors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/recipeDB', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Connected to mongodb");
@@ -18,7 +20,8 @@ app.get('/', (req, res) =>{
 
 const Recipe = require("./models/recipe")
 app.get("/getRecipes", async (req, res) =>{
-    const recipes = await Recipe.find().then(()=>res.json())
+    const recipes = await Recipe.find()
+    res.json(recipes);
     
     res.render('recipes',{ recipes });
 })
@@ -33,9 +36,13 @@ app.put('/recipe/update/:id',async (req,res) =>{
     const recipe = await Recipe.findByIdAndUpdate(req.params.id,req.body)
     recipe.save().then(()=>res.json())
 })
+//delete
+
+app.delete('/recipe/delete/:id',async (req,res) =>{
+    const recipe = await Recipe.findByIdAndDelete(req.params.id)
+    recipe.save().then(()=>res.json())
+})
   
-
-
 app.set('view engine', 'ejs');
 app.listen(3001,()=>{
     console.log('server is running on port 3001')

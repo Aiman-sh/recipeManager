fa515fae19c6d9eea6e42d85913199e1cc66bb8d
Aiman <aiman.bscsf19@iba-suk.edu.pk>
2023-06-24 15:36:16 +0500

    Added server side code for adding and displaying

--- server.js
+++ server.js
@@ -8,11 +8,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/recipeDB', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+    console.log("Connected to mongodb");
+})
 
 app.get('/', (req, res) =>{
-    res.render('index')
+    res.render('addRecipe')
 });
+
+const Recipe = require("./models/recipe")
+app.get("/getRecipes", async (req, res) =>{
+    const recipes = await Recipe.find().then(()=>res.json())
+    
+    res.render('recipes',{ recipes });
+})
+app.post('/recipe/add',async (req,res) =>{
+    const recipe = await Recipe.create(req.body)
+    recipe.save().then(()=>res.redirect("recipes"))
+})
 
 
 app.set('view engine', 'ejs');
